7ff847ad6f37e2f0a38c96beca5471e000e92719
Ermindo Lopes Neto <ermindo.lopes@gmail.com>
2023-11-26 17:09:56 -0300

    ajustes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  
+  const logger = new Logger("API");
+  const port = parseInt(process.env.PORT) || 3000;
+
+  app.enableCors({ origin: '*', methods: '*', allowedHeaders: '*', preflightContinue: false });
+  
+  await app.listen(
+    port,
+    () =>{      
+      logger.log("Backend rodando na porta: "+ port);
+    }
+  );
+
+
 }
 bootstrap();
