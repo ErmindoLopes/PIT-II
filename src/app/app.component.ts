ed766f849c45fa1b1e2ed0692206c6ddc31a2a97
Ermindo Lopes Neto <ermindo.lopes@gmail.com>
2023-11-30 20:07:52 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,10 @@
 import { Component } from '@angular/core';
-import { SessionStorageService } from 'ngx-webstorage';
-import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'PIT-II-Front';
-
-  /**
-   *
-   */
-  constructor(
-    private sessionStorage: SessionStorageService,
-  ) {
-
-    this.init();
-
-  }
-
-
-  init(){
-
-
-    // create agentId
-    /* ********************************************************************** */
-    // verifica se já existe agentId e token. Se não existir cria, caso contrario não.
-    if (!this.sessionStorage.retrieve('agentId') && !this.sessionStorage.retrieve('userToken')){
-      const agentId = uuid();
-      this.sessionStorage.store('agentId', agentId);
-    }
-    /* ********************************************************************** */
-
-
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,20 @@
-import localeBr from '@angular/common/locales/pt';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { registerLocaleData } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { LuxonDateAdapter, MAT_LUXON_DATE_ADAPTER_OPTIONS, MAT_LUXON_DATE_FORMATS } from '@angular/material-luxon-adapter';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
-import { NgxWebstorageModule } from 'ngx-webstorage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
-
-
-
-
-
-registerLocaleData(localeBr);
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-
-    RouterModule,
     AppRoutingModule,
-
-    HttpClientModule,
-
-    NgxWebstorageModule.forRoot({ prefix: 'PIT2', separator: '.', caseSensitive: true }),
+    BrowserAnimationsModule
   ],
-  providers: [
-    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }, // para mat-datepicker
-    { provide: MAT_LUXON_DATE_ADAPTER_OPTIONS, useValue: { strict: true } }, // para mat-datepicker
-    { provide: DateAdapter, useClass: LuxonDateAdapter, deps: [MAT_DATE_LOCALE, MAT_LUXON_DATE_ADAPTER_OPTIONS] }, // para mat-datepicker
-    { provide: MAT_DATE_FORMATS, useValue: MAT_LUXON_DATE_FORMATS }, // para mat-datepicker
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-
-}
+export class AppModule { }
